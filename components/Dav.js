758182d5fb8d070d189dff9aba2d0905eee8b7c9
dvdapogeo <dvdingles@hotmail.com>
2022-10-18 08:01:22 +0200

    third

--- components/Dav.js
+++ components/Dav.js
@@ -6,9 +6,54 @@ import React, { useRef } from "react";
 import { useGLTF } from "@react-three/drei";
 
 export function Dav(props) {
-  const { nodes, materials } = useGLTF("/4D ANT FER.gltf");
+  const { nodes, materials } = useGLTF("/5D ANT FER.gltf");
   return (
     <group {...props} dispose={null}>
+      <mesh
+        castShadow
+        receiveShadow
+        geometry={nodes.Plano002.geometry}
+        material={materials["Material.001"]}
+        position={[0.31, 0.14, -0.07]}
+      />
+      <mesh
+        castShadow
+        receiveShadow
+        geometry={nodes.Plano003.geometry}
+        material={materials["Material.001"]}
+        position={[0.31, 0.14, -0.07]}
+      />
+      <mesh
+        castShadow
+        receiveShadow
+        geometry={nodes.Plano004.geometry}
+        material={materials["Material.001"]}
+        position={[0.24, 0.14, -0.07]}
+      />
+      <mesh
+        castShadow
+        receiveShadow
+        geometry={nodes.Texto.geometry}
+        material={materials["Material.001"]}
+        position={[0.24, -0.07, 0.23]}
+        rotation={[Math.PI / 2, 0, 0]}
+      />
+      <mesh
+        castShadow
+        receiveShadow
+        geometry={nodes.Texto001.geometry}
+        material={materials["Material.001"]}
+        position={[-0.1, -0.07, 0.09]}
+        rotation={[Math.PI / 2, 0, 0]}
+      />
+      <mesh
+        castShadow
+        receiveShadow
+        geometry={nodes.Texto002.geometry}
+        material={materials["Material.001"]}
+        position={[0.49, -0.05, 0.21]}
+        rotation={[Math.PI / 2, 0, 0]}
+      />
       <group position={[-0.28, -0.02, 0.05]} rotation={[0, 0.44, 0]}>
         <mesh
           castShadow
@@ -53,4 +98,4 @@ export function Dav(props) {
   );
 }
 
-useGLTF.preload("/4D ANT FER.gltf");
+useGLTF.preload("/5D ANT FER.gltf");
